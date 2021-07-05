3a52600eb5b8869126db7929c7d7d5e52e444ed4
patnaapp <patnaapp@gmail.com>
2021-07-05 17:24:01 +0530

    First3.07.21

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -66,72 +66,79 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
 
-      backgroundColor: Color.fromARGB(255, 20, 134, 147),
+     // backgroundColor: Color.fromARGB(255, 20, 134, 147),
 
-      body: SafeArea(
+      body: Container(
+      decoration: BoxDecoration(
+      image: DecorationImage(
+      image: AssetImage("assets/images/bg.png"),
+      fit: BoxFit.cover,
+      ),),
+        child: SafeArea(
 
-        top: false,
-        bottom: false,
-       child: Form(
-        autovalidate: true,
+          top: false,
+          bottom: false,
+         child: Form(
+          autovalidate: true,
 
-        child: SingleChildScrollView(
-        padding: const EdgeInsets.only(top: 50.0,bottom: 40.0),
-        child: Column(
+          child: SingleChildScrollView(
+          padding: const EdgeInsets.only(top: 50.0,bottom: 40.0),
+          child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-            child:new Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
+              Container(
+              child:new Row(
+              children: <Widget>[
 
-              new Padding(padding: new EdgeInsets.only(left: 20.0 ),),
+                new Padding(padding: new EdgeInsets.only(left: 20.0 ),),
 
-            Image.asset("images/eleclogo.png",
-                width: 50.0,
-                height: 50.0,
+              Image.asset("images/eleclogo.png",
+                  width: 50.0,
+                  height: 50.0,
 
-                fit: BoxFit.cover),
+                  fit: BoxFit.cover),
 
-              new Padding(padding: new EdgeInsets.only(right: 90.0 ),),
-             ],),),
+                new Padding(padding: new EdgeInsets.only(right: 90.0 ),),
+               ],),),
 
-            Image.asset("images/namaskaar_splash.png",
+              Image.asset("images/namaskaar_splash.png",
+                  width: 200.0,
+                  height: 50.0,
+              ),
+
+              /*Image.asset("assets/dart.png", width: 150.0, height: 150,),*/
+              Container(margin: EdgeInsets.all(50.0),
+                child: Image.asset("images/logo_Voter.png",
                 width: 200.0,
-                height: 50.0,
+                height: 200.0,
+              )
+                , ),
+              Text('Welcome to One Point Source of',
+                style: TextStyle(fontSize: 15.0,color: Colors.white,),),
+              Text(' Information & Services for Voters in India',
+                style: TextStyle(fontSize: 15.0,color: Colors.white,),),
+
+
+
+
+          new Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.only(top: 20.0),
+            child:new Image.asset("images/niclogo.png",
+                  width: 140.0,
+                  height: 40.0,
+
+                  fit: BoxFit.cover),),
+
+
+
+
+
+             ],
             ),
-
-            /*Image.asset("assets/dart.png", width: 150.0, height: 150,),*/
-            Container(margin: EdgeInsets.all(50.0),
-              child: Image.asset("images/logo_Voter.png",
-              width: 200.0,
-              height: 200.0,
-            )
-              , ),
-            Text('Welcome to One Point Source of',
-              style: TextStyle(fontSize: 15.0,color: Colors.white,),),
-            Text(' Information & Services for Voters in India',
-              style: TextStyle(fontSize: 15.0,color: Colors.white,),),
-
-
-
-
-        new Container(
-          alignment: Alignment.bottomRight,
-          margin: const EdgeInsets.only(top: 20.0),
-          child:new Image.asset("images/niclogo.png",
-                width: 140.0,
-                height: 40.0,
-
-                fit: BoxFit.cover),),
-
-
-
-
-
-           ],
+           ),
           ),
-         ),
         ),
       ),
     );
